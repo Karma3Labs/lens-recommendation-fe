7c4ee748923daca5365c7c47c602c8378d267e5e
Seungju Lee <sjtogo@gmail.com>
2023-11-09 20:21:33 +0300

    fix: remove reference to env variables on frontend

--- app/env.ts
+++ app/env.ts
@@ -1,7 +1,7 @@
 export function getEnv() {
     return {
-        PROFILE_URL: process.env.PROFILE_URL || "https://lens.k3l.io",
-        CONTENT_URL: process.env.CONTENT_URL || "https://content.lens.k3l.io",
+        PROFILE_URL: "https://lens.k3l.io",
+        CONTENT_URL: "https://content.lens.k3l.io",
     }
 }
 
